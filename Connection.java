a28c77317d0020ab58ab13f64f27efda27cda054
Ellen <eli15@ocdsb.ca>
2026-05-08 11:26:24 -0400

    create classes

---
+++ Connection.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Connection here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Connection
+{
+}
---
+++ House.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class House here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class House
+{
+}
