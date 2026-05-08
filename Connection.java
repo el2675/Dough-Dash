c472dfc35d842d28d67f41d45becc4437e21d0c0
Kevin <ksand1@ocdsb.ca>
2026-05-08 11:44:15 -0400

    Connection class completed

--- Connection.java
+++ Connection.java
@@ -1,10 +1,21 @@
 
 /**
- * Write a description of class Connection here.
+ * Defines a connection between two locations with an 
+ * index and travel time. The index is used to access
+ * a location through locations in the map.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Kevin Sander
+ * May 8th, 2026
  */
 public class Connection
 {
+    byte bytIndex;
+    byte bytTravelTime;
+    
+    Connection(byte i, byte tt){
+        this.bytIndex = i;
+        this.bytTravelTime = tt;
+    }
+    
+    
 }
