8871aff486b1832a4a0c5c8f7d193fd93d0f0cb1
Ellen <eli15@ocdsb.ca>
2026-05-09 20:17:24 -0400

    update

--- GUI.java
+++ GUI.java
@@ -9,7 +9,10 @@
 //import java swing for GUI
 import javax.swing.*;
 
-public class GUI extends JFrame
+//import arraylist
+import java.util.ArrayList;
+
+public class GUI extends JFrame implements ActionListener
 {
     //create a constructor for the GUI
     GUI(){
@@ -18,5 +21,26 @@ public class GUI extends JFrame
         
         //set the frame size
         this.setSize(600, 400);
+        
+        //create arraylist of buttons
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
+        
+        //create a button for each location
+        for(byte i = 0; i < numLocations(); i++){
+            //create a JButton 
+            buttons.add(new JButton());
+            
+            //set button position on map
+            locations.get(i).bytXCoordinate;
+            
+        }
+        
+        
+    }
+    
+    //create method for action listener
+    public void onCLick(ActionEvent e){
+        //check if the house was waiting for order
+        
     }
 }
