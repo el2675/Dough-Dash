053d2793d9ce32eca08cffa4acdab932a19c64ab
Kevin <ksand1@ocdsb.ca>
2026-05-12 10:37:23 -0400

    Connection Class completed, hard coded version of the Location Class completed.

--- Connection.java
+++ Connection.java
@@ -9,13 +9,21 @@
  */
 public class Connection
 {
+    //declare instance variables
+    
+    //declare variable for the index
+    //of the other location in the
+    //locations array list
     byte bytIndex;
+    
+    //declare variable for the travel
+    //time between the connected 
+    //locations
     byte bytTravelTime;
     
+    //define constuctor
     Connection(byte i, byte tt){
         this.bytIndex = i;
         this.bytTravelTime = tt;
     }
-    
-    
 }
