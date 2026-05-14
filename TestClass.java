0edaa5dcddb924b5d85b8925bc307cf80726ece3
Kevin <ksand1@ocdsb.ca>
2026-05-14 10:32:08 -0400

    Added a test class.

--- TestClass.java
+++ TestClass.java
@@ -1,33 +1,17 @@
 
 /**
- * Write a description of class TestClass here.
+ * Test class to start the program from
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Kevin Sander)
+ * @version (May 14, 2026)
  */
 public class TestClass
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class TestClass
-     */
-    public TestClass()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String args[]){
+        //create driver to run the game
+        Driver driver = new Driver();
+        
+        //start the game
+        driver.run();
     }
 }
