36369e4d186f0bf1c20c917266e2ef9049930efa
Kevin <ksand1@ocdsb.ca>
2026-05-13 23:47:41 -0400

    Move method added and implemented for Driver Class.

--- Driver.java
+++ Driver.java
@@ -5,6 +5,10 @@
  * @author (Ellen Li, Kevin Sander)
  * @version (May 8, 2026)
  */
+
+//import JOptionPane for user input
+import javax.swing.JOptionPane;
+
 public class Driver
 {
     //declare instance variables to store the game time
@@ -29,8 +33,78 @@ public class Driver
         GUI gui = new GUI(map, this);
     }
     
+    //handles button clicks, this usually results in a move
+    public void move(byte bytIndex, Player player, Map map){
+        //checking if the clicked location is connected
+        //to the player's location
+        
+        //declare and initialize variable for
+        //the player's location
+        Location playerLocation = map.getLocation(player.bytLocation);
+        
+        //declare and initialize variable to
+        //store the index the connected
+        //location was found
+        byte bytIndexConnected = -1;
+        
+        //loop through locations connected
+        //to the player's location
+        for(byte i = 0; i < playerLocation.connectedLocations.size(); i++){
+            //check if current connection's index
+            //matchs that of the clicked location
+            if(playerLocation.connectedLocations.get(i).bytIndex == bytIndex){
+                //set bolConnected to reflect
+                //that the connection was found
+                bytIndexConnected = i;
+                
+                //exiting the loop because the 
+                //connection was found
+                break;
+            }
+        }
+        
+        //exit the method if a connection
+        //was not found
+        if(bytIndexConnected == -1){
+            //exiting method
+            return;
+        }
+        
+        //declare and initialize variable to
+        //store the clicked location
+        Location clickedLocation = map.getLocation(bytIndex);
+        
+        //move player
+        player.move(bytIndex, clickedLocation.bytXCoordinate, clickedLocation.bytYCoordinate);
+        
+        //update player score
+        player.bytScore += clickedLocation.connectedLocations.get(bytIndexConnected).bytTravelTime;
+        
+        //check if the clicked location
+        //is a house waiting for and order
+        if(clickedLocation instanceof House && ((House)clickedLocation).bolWaitingForOrder){
+            player.bytNumDeliveries--;
+        }
+        
+        //check for win
+        if(player.checkWin()){
+            //prompt user if they want to play
+            //again
+            if(JOptionPane.showConfirmDialog(null,"Do you want to play again?", "Yes or No", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+                //reset the game
+                this.reset();
+            }
+            
+            //exit the program because the 
+            //user does not want to play
+            //again
+            System.exit(0);
+        }
+    }
+    
     //create a method to reset the game
     public void reset(){
+        //start a new game
         this.run();        
     }
     
